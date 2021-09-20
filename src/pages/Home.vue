05eebe957b6c987468b478351979d41a0bf04af7
Nikita <nikita_sokolov98@mail.ru>
2021-09-20 13:49:55 +0300

    added text about the app

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="wrapper-content wrapper-content--fixed">
         <section>
-            <div class="container">
-                <h1 class="title">Главная страница</h1>
-                <p>Приложение Shop, в котором были применены знания vue-router'а и vuex.</p><br>
-                <p>Массив с продуктами хранится в Store.</p><br>
-                <a class="link" href="#/shop">Перейти в каталог</a>
+            <div class="container" style="text-align:center;">
+                <h1 class="title">В этом приложении:</h1>
+                <ul class="func-list">
+                    <li>Работа с Vue-Router;</li>
+                    <li>Создание динамических маршрутов;</li>
+                    <li>Хранение и взаимодействие с данных через Vuex;</li>
+                    <li>Разница между actions и mutations;</li>
+                    <li>Работа с общим стейтом приложения;</li>
+                </ul>
+                <a href="#/shop/" class="btn btnPrimary">Посмотреть Демо</a>
             </div>
         </section>
     </div>
 </template>
+
+<style lang="scss" scoped>
+ul.func-list {
+    margin-bottom: 30px;
+
+    li {
+        margin-bottom: 20px;
+    }
+}
+</style>
